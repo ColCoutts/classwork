6f1302f9e3aff067978296fe282d238cba449fa1
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-02 12:00:28 -0700

    passing animalconstructor tests

--- 2_objects/lib/index.js
+++ 2_objects/lib/index.js
@@ -3,6 +3,10 @@ function AnimalConstructor(name, species) {
     this.species = species;
 }
 
+AnimalConstructor.prototype.speak = function() {
+    return `my name is ${this.name} and i am a ${this.species}`;
+};
+
 module.exports = {
     AnimalConstructor
 };
--- 2_objects/test/index.test.js
+++ 2_objects/test/index.test.js
@@ -5,4 +5,12 @@ describe('animal constructor function', ()=> {
         const dog = new AnimalConstructor('rover');
         expect(dog.name).toEqual('rover');
     });
+    it('has name and species property', ()=> {
+        const dog = new AnimalConstructor('rover', 'dog');
+        expect(dog).toEqual({ 'name':'rover', 'species':'dog' });
+    });
+    it('speak method display name and species', ()=> {
+        const dog = new AnimalConstructor('rover', 'dog');
+        expect(dog.speak()).toEqual('my name is rover and i am a dog');
+    });
 });
