0a9989d2427c6e26978ff8f7f504dea86a5ff06c
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-04-02 12:54:36 -0700

    pass factory tests

--- 2_objects/lib/index.js
+++ 2_objects/lib/index.js
@@ -1,3 +1,5 @@
+//constructor functions
+
 function AnimalConstructor(name, species) {
     this.name = name;
     this.species = species;
@@ -7,6 +9,20 @@ AnimalConstructor.prototype.speak = function() {
     return `my name is ${this.name} and i am a ${this.species}`;
 };
 
+//factory functions
+
+const animalFactory = function(name, species) {
+    return {
+        name,
+        species,
+        speak() {
+            return `hi my name is ${this.name} and i am a ${this.species}`;
+        }
+    };
+};
+
+
 module.exports = {
-    AnimalConstructor
+    AnimalConstructor,
+    animalFactory
 };
--- 2_objects/test/index.test.js
+++ 2_objects/test/index.test.js
@@ -1,16 +1,32 @@
-const { AnimalConstructor } = require('../lib/index.js');
+const { AnimalConstructor, animalFactory } = require('../lib/index.js');
 
-describe('animal constructor function', ()=> {
+describe('animal constructor function', () => {
     it('has name property', ()=> {
         const dog = new AnimalConstructor('rover');
         expect(dog.name).toEqual('rover');
     });
-    it('has name and species property', ()=> {
+    it('has name and species property', () => {
         const dog = new AnimalConstructor('rover', 'dog');
         expect(dog).toEqual({ 'name':'rover', 'species':'dog' });
     });
-    it('speak method display name and species', ()=> {
+    it('speak method display name and species', () => {
         const dog = new AnimalConstructor('rover', 'dog');
         expect(dog.speak()).toEqual('my name is rover and i am a dog');
     });
+    describe('animal factory function', () => {
+        it('has name property', () => {
+            const dog = animalFactory('rover');
+            expect(dog.name).toEqual('rover');
+        });
+        it('has species property', () => {
+            const dog = animalFactory('rover', 'dog');
+            expect(dog.species).toEqual('dog');
+        });
+        it('speak method to display name/species', () => {
+            const dog = animalFactory('rover', 'dog');
+            expect(dog.speak()).toEqual('hi my name is rover and i am a dog');
+        });
+    });
+    
+
 });
